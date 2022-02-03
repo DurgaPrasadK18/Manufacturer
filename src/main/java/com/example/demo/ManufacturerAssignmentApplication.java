64fb8bc7b499196ec306ab79404c3fc5ce9a716c
DurgaPrasadK18 <durgaprasadkdp18@gmail.com>
2022-02-03 12:26:50 +0530

    first commit

---
+++ src/main/java/com/example/demo/ManufacturerAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManufacturerAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManufacturerAssignmentApplication.class, args);
+	}
+
+}
